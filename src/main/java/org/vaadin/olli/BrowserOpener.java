ae39df8e1eacf00fd61f21d57fed9efac333a2dd
Olli Tietäväinen <ollit@vaadin.com>
2018-09-26 13:08:08 +0300

    added possibility of configuring windowName property + javadoc

--- src/main/java/org/vaadin/olli/BrowserOpener.java
+++ src/main/java/org/vaadin/olli/BrowserOpener.java
@@ -8,6 +8,9 @@ import com.vaadin.flow.component.polymertemplate.Id;
 import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
 import com.vaadin.flow.templatemodel.TemplateModel;
 
+/**
+ * Wrapper component that makes other components links that open browser windows.
+ */
 @Tag("browser-opener")
 @HtmlImport("browser-opener.html")
 public class BrowserOpener extends PolymerTemplate<BrowserOpener.BrowserOpenerModel> {
@@ -15,10 +18,36 @@ public class BrowserOpener extends PolymerTemplate<BrowserOpener.BrowserOpenerMo
     @Id
     Anchor content;
 
+    public BrowserOpener() {
+        getModel().setWindowName("_blank");
+    }
+
+    /**
+     * Set the address to open.
+     *
+     * @param url
+     *     URL address BrowserOpener should navigate to
+     */
     public void setUrl(String url) {
         getModel().setUrl(url);
     }
 
+    /**
+     * Name of the window to open. Defaults to @code{_blank}, which opens a new tab.
+     *
+     * @param windowName
+     *     name of the window
+     */
+    public void setWindowName(String windowName) {
+        getModel().setWindowName(windowName);
+    }
+
+    /**
+     * Set the component to wrap. Clicking this component will navigate to the url specified in `setUrl`
+     *
+     * @param component
+     *     The component to wrap.
+     */
     public void setContent(Component component) {
         this.content.removeAll();
         this.content.add(component);
@@ -28,5 +57,9 @@ public class BrowserOpener extends PolymerTemplate<BrowserOpener.BrowserOpenerMo
         String getUrl();
 
         void setUrl(String url);
+
+        String getWindowName();
+
+        void setWindowName(String windowName);
     }
 }
--- src/test/java/org/vaadin/olli/DemoView.java
+++ src/test/java/org/vaadin/olli/DemoView.java
@@ -2,15 +2,24 @@ package org.vaadin.olli;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route("")
 public class DemoView extends Div {
 
     public DemoView() {
+
         BrowserOpener bo = new BrowserOpener();
-        bo.setContent(new Button("click this button to open"));
+        bo.setContent(new Button("click this button to open a new window"));
         bo.setUrl("https://www.vaadin.com");
-        add(bo);
+
+        BrowserOpener bo2 = new BrowserOpener();
+        bo2.setContent(new Button("click this button open page in this window"));
+        bo2.setUrl("https://www.google.com");
+        bo2.setWindowName("_self");
+        VerticalLayout vl = new VerticalLayout(bo, bo2);
+        add(vl);
+
     }
 }
